7dc5b0fb1536e5eac918243bb20e886a6657a0c9
Jordan Rhodes <jsrhodes15@gmail.com>
2018-04-08 01:05:33 -0400

    Start production version of docker-compose file. Add retry logic on startup when unable to connect to db

--- server/index.js
+++ server/index.js
@@ -5,6 +5,45 @@ const dbConfig = {
   name: "cryptoklout",
   url: "mongodb://mongodb:27017"
 };
+const dbOptions = {
+  reconnectTries: 30,
+  reconnectInterval: 1000
+};
+
+function createConnection(dbURL, options) {
+  var db = mongoose.createConnection(dbURL, options);
+
+  db.on("error", function(err) {
+    // If first connect fails because mongod is down, try again later.
+    // This is only needed for first connect, not for runtime reconnects.
+    // See: https://github.com/Automattic/mongoose/issues/5169
+    if (
+      err.message &&
+      err.message.match(/failed to connect to server .* on first connect/)
+    ) {
+      console.log(new Date(), String(err));
+
+      // Wait for a bit, then try to connect again
+      setTimeout(function() {
+        console.log("Retrying first connect...");
+        db.openUri(dbURL).catch(() => {});
+        // Why the empty catch?
+        // Well, errors thrown by db.open() will also be passed to .on('error'),
+        // so we can handle them there, no need to log anything in the catch here.
+        // But we still need this empty catch to avoid unhandled rejections.
+      }, 20 * 1000);
+    } else {
+      // Some other error occurred.  Log it.
+      console.error(new Date(), String(err));
+    }
+  });
+
+  db.once("open", function() {
+    console.log("Connection to db established.");
+  });
+
+  return db;
+}
 
 const app = express();
 
@@ -12,15 +51,33 @@ const port = 3002;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(dbConfig.url, (err, client) => {
-  if (err) {
-    return console.log(err);
-  }
-  const db = client.db(dbConfig.name);
+/**
+ * Attempt to connect to MongoDB, retry on first attempt because the driver does not do this for us.
+ */
+function connectWithRetry() {
+  return MongoClient.connect(dbConfig.url, (err, client) => {
+    if (err) {
+      if (
+        err.message &&
+        err.message.match(/failed to connect to server .* on first connect/)
+      ) {
+        console.warn(
+          "Failed to connect to mongo on startup - retrying in 5 sec"
+        );
+        setTimeout(connectWithRetry, 5000);
+      } else {
+        console.error("Something went terribly wrong connecting to mongo", err);
+      }
+    } else {
+      const db = client.db(dbConfig.name);
 
-  require("./app/routes")(app, db);
+      require("./app/routes")(app, db);
 
-  app.listen(port, () => {
-    console.log("We are live on " + port);
+      app.listen(port, () => {
+        console.log("We are live on " + port);
+      });
+    }
   });
-});
+}
+
+connectWithRetry();
