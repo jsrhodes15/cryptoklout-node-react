c26646e43fafdde6ed04356b26b578b2eb48b039
Jordan Rhodes <jsrhodes15@gmail.com>
2018-04-29 00:47:25 -0400

    Remove stubbed code

--- server/router/index.js
+++ server/router/index.js
@@ -11,8 +11,6 @@ router.get('/', function(req, res) {
   res.send('Users home page');
 });
 // define the about route
-router.get('/about', function(req, res) {
-  res.send('About users');
-});
-
+router.route('/signup')
+  .post(authentication.signup)
 module.exports = router;
