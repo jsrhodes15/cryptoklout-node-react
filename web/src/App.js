02bdb5cc2c47ac2537c6933b564dbbe722779216
overwatch13 <williamhowley@gmail.com>
2018-05-08 09:23:33 -0400

    commit to pull latest

--- web/src/App.js
+++ web/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// App.js is the entry point.
+// App.js should be more of a route tree. 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Reacttt</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
